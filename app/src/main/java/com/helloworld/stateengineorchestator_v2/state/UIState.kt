326127657e79171b16e9a81c913cfdac60ad6154
codex-yasxh <adit7ya0patanwar@outlook.com>
2026-02-07 10:02:41 +0530

    defined states for the contracts

---
+++ app/src/main/java/com/helloworld/stateengineorchestator_v2/state/UIState.kt
@@ -0,0 +1,11 @@
+package com.helloworld.stateengineorchestator_v2.state
+
+sealed class UIState{
+    object Idle : UIState(),
+
+    object Loading : UIState(),
+
+    data class Success(val data: String) : UIState(),
+
+    data class Error(val reason: Throwable) : UIState()
+}
